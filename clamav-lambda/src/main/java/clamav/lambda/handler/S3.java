84462a869c208a40dbc71c4bd09109d56a0f4598
Hardik <hardikgw@gmail.com>
2021-04-07 15:54:38 -0400

    meta, tags updates

--- clamav-lambda/src/main/java/clamav/lambda/handler/S3.java
+++ clamav-lambda/src/main/java/clamav/lambda/handler/S3.java
@@ -15,7 +15,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.File;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.atomic.AtomicReference;
